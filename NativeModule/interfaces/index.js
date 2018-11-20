f0bb297563d2a698006ca4eea548f3f3a686b792
LCD27 <756355668@qq.com>
2018-11-20 15:02:48 +0800

    Update index.js
    添加在线模块索引
    
    Co-Authored-By: lcd27 <lcd27@users.noreply.github.com>

--- NativeModule/interfaces/index.js
+++ NativeModule/interfaces/index.js
@@ -2,10 +2,13 @@ import SMap from './SMap'
 import SAnalyst from './SAnalyst'
 import SCollector from './SCollector'
 import SScene  from './SScene'
-
+import {SOnlineService} from './iServer'
 export {
   SMap,
   SAnalyst,
   SCollector,
   SScene,
+
+  /*在线模块功能*/
+  SOnlineService,
 }
