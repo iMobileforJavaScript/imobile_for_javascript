6befedb17200d8fbb06d70ce5a935e72903b78ad
LCD27 <756355668@qq.com>
2018-11-20 14:36:32 +0800

    还原.gradle
    添加SOnlineService模块
    
    Co-Authored-By: lcd27 <lcd27@users.noreply.github.com>

--- NativeModule/interfaces/index.js
+++ NativeModule/interfaces/index.js
@@ -3,9 +3,13 @@ import SAnalyst from './SAnalyst'
 import SCollector from './SCollector'
 import SScene  from './SScene'
 
+import {SOnlineService} from './iServer'
 export {
   SMap,
   SAnalyst,
   SCollector,
   SScene,
+
+  /*iServer  add by lucd*/
+  SOnlineService,
 }
