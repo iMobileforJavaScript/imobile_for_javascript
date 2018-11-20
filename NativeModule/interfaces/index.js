f82ab34181d45bd19cd801e659e16645ec250cfe
LCD27 <756355668@qq.com>
2018-11-20 14:41:16 +0800

    Revert "还原.gradle"
    This reverts commit 6befedb17200d8fbb06d70ce5a935e72903b78ad.

--- NativeModule/interfaces/index.js
+++ NativeModule/interfaces/index.js
@@ -3,13 +3,9 @@ import SAnalyst from './SAnalyst'
 import SCollector from './SCollector'
 import SScene  from './SScene'
 
-import {SOnlineService} from './iServer'
 export {
   SMap,
   SAnalyst,
   SCollector,
   SScene,
-
-  /*iServer  add by lucd*/
-  SOnlineService,
 }
