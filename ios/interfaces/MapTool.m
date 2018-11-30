4653c1292e714e550ea4969d75c1d5ad484e7611
DragonYang <dragonyang0917@gmail.com>
2018-11-30 09:55:24 +0800

    去掉调试日志

---
+++ ios/interfaces/MapTool.m
@@ -0,0 +1,13 @@
+//
+//  MapTool.m
+//  Supermap
+//
+//  Created by Shanglong Yang on 2018/11/29.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "MapTool.h"
+
+@implementation MapTool
+
+@end
