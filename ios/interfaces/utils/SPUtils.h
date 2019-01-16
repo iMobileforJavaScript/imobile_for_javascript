2a440325162c955d53ade298d15763bb5dc1d0f8
xianglong li <383002593@qq.com>
2019-01-16 19:03:54 +0800

    ios设置工具类

---
+++ ios/interfaces/utils/SPUtils.h
@@ -0,0 +1,13 @@
+//
+//  SPUtils.h
+//  Supermap
+//
+//  Created by xianglong li on 2019/1/15.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPUtils : NSObject
+
+@end
---
+++ ios/interfaces/utils/SPUtils.m
@@ -0,0 +1,13 @@
+//
+//  SPUtils.m
+//  Supermap
+//
+//  Created by xianglong li on 2019/1/15.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "SPUtils.h"
+
+@implementation SPUtils
+
+@end
