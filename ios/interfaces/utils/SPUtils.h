1b7b2919f9a78a7f93c01508c784dea1bb671e74
xianglong li <383002593@qq.com>
2019-01-17 14:45:41 +0800

    修改制图缺陷及专题图接口

--- ios/interfaces/utils/SPUtils.h
+++ ios/interfaces/utils/SPUtils.h
@@ -1,13 +1,14 @@
 //
 //  SPUtils.h
 //  Supermap
-//
+//  读取和写入设置参数xml
 //  Created by xianglong li on 2019/1/15.
 //  Copyright © 2019 Facebook. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import "SuperMap/SuperMap.h"
 
-@interface SPUtils : NSObject
+@interface SPUtils : NSObject<RCTBridgeModule>
 
 @end
--- ios/interfaces/utils/SPUtils.m
+++ ios/interfaces/utils/SPUtils.m
@@ -7,7 +7,71 @@
 //
 
 #import "SPUtils.h"
+#import "SuperMap/SuperMap.h"
 
 @implementation SPUtils
+RCT_EXPORT_MODULE();
 
+//RCT_REMAP_METHOD(createFile, createFileWithResolver:(NSString*) fileName resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//        resolve([NSNumber numberWithBool:true]);
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
+//
+//RCT_REMAP_METHOD(createThemeUniqueMap, createThemeUniqueMapWithResolver:(NSDictionary*) dataDic resolve:(RCTPromiseResolveBlock) resolve reject:(RCTPromiseRejectBlock) reject){
+//    @try{
+//
+//    }
+//    @catch(NSException *exception){
+//        reject(@"workspace", exception.reason, nil);
+//    }
+//}
 @end
