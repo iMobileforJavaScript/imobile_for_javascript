80b4edbe5c78eb4ab19dc9f94de0236d9cb854ba
xiezhiyan <xiezhiyanfighting@163.com>
2019-03-29 18:10:24 +0800

    处理编译不过
    处理编译不过

--- android/src/main/java/com/supermap/interfaces/mapping/SMap.java
+++ android/src/main/java/com/supermap/interfaces/mapping/SMap.java
@@ -735,7 +735,7 @@ public class SMap extends ReactContextBaseJavaModule {
             style.fromJson(styleJson);
             selection.setStyle(style);
 
-            promise.resolve(true);setLabelColor
+            promise.resolve(true);
         } catch (Exception e) {
             promise.reject(e);
         }
