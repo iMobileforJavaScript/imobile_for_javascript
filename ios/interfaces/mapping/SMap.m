6130ebba83a294e56ba815940c174c2a5e562170
xiezhiyan <xiezhiyanfighting@163.com>
2019-04-19 16:40:59 +0800

    标注提交
    标注提交

--- ios/interfaces/mapping/SMap.m
+++ ios/interfaces/mapping/SMap.m
@@ -1916,9 +1916,39 @@ RCT_REMAP_METHOD(openTaggingDataset, openTaggingDatasetWithPath:(NSString *)user
                 Map *map = sMap.smMapWC.mapControl.map;
                 for(int i = 0, count = datasets.count; i < count; i++){
                     Dataset *ds = [datasets get:i];
-                    Layer *layer = [map.layers addDataset:ds ToHead:YES];
-                    layer.editable = NO;
-                    layer.visible = NO;
+                    NSString* addname = [ds.name stringByAppendingFormat:@"@Label_%@#",userpath]; //getName()+"@Label_"+userpath+"#";
+                    BOOL add = true;
+                    Layers* maplayers = map.layers;
+                    for(int j=0 ; j<[maplayers getCount];j++){
+                        if([[maplayers getLayerAtIndex:j].caption isEqualToString:addname]){
+                            add = false;
+                        }
+                    }
+                    if(add) {
+                        Layer* layer = [map.layers addDataset:ds ToHead:YES];// .add(ds, true);
+                        layer.editable = false;// setEditable(false);
+                        layer.visible = false;//(false);
+                    }
+                }
+            }
+            resolve(@(YES));
+        }else {
+            Datasets *datasets = opendatasource.datasets;
+            Map *map = sMap.smMapWC.mapControl.map;
+            for(int i = 0, count = datasets.count; i < count; i++){
+                Dataset *ds = [datasets get:i];
+                NSString* addname = [ds.name stringByAppendingFormat:@"@Label_%@#",userpath]; //getName()+"@Label_"+userpath+"#";
+                BOOL add = true;
+                Layers* maplayers = map.layers;
+                for(int j=0 ; j<[maplayers getCount];j++){
+                    if([[maplayers getLayerAtIndex:j].caption isEqualToString:addname]){
+                        add = false;
+                    }
+                }
+                if(add) {
+                    Layer* layer = [map.layers addDataset:ds ToHead:YES];// .add(ds, true);
+                    layer.editable = false;// setEditable(false);
+                    layer.visible = false;//(false);
                 }
             }
             resolve(@(YES));
