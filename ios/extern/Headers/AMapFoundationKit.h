595c8ef674985fac094b3f3498308c338b48ea6f
xiezhiyan <xiezhiyanfighting@163.com>
2019-06-04 17:22:55 +0800

    IOS集成第三方定位
    IOS集成第三方定位

---
+++ ios/extern/Headers/AMapFoundationKit.h
@@ -0,0 +1,15 @@
+//
+//  AMapFoundationKit.h
+//  AMapFoundationKit
+//
+//  Created by xiaoming han on 15/10/28.
+//  Copyright © 2015年 Amap. All rights reserved.
+//
+
+#import "AMapFoundationVersion.h"
+#import "AMapServices.h"
+#import "AMapURLSearchConfig.h"
+#import "AMapURLSearchType.h"
+#import "AMapURLSearch.h"
+
+#import "AMapUtility.h"
---
+++ ios/extern/Headers/AMapLocationKit.h
@@ -0,0 +1,18 @@
+//
+//  AMapLocationKit.h
+//  AMapLocationKit
+//
+//  Created by AutoNavi on 15/10/22.
+//  Copyright © 2015年 Amap. All rights reserved.
+//
+
+
+#import "AMapLocationVersion.h"
+
+#import "AMapLocationManager.h"
+#import "AMapLocationCommonObj.h"
+#import "AMapLocationRegionObj.h"
+
+#import "AMapGeoFenceRegionObj.h"
+#import "AMapGeoFenceManager.h"
+#import "AMapGeoFenceError.h"
