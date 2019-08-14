36dd3b0a077b4ea6cde7681d2e47c37eee90c519
xiezhiyan <xiezhiyanfighting@163.com>
2019-08-14 16:14:01 +0800

    标绘动画提交
    标绘动画提交

--- ios/SuperMap/AnimationWay.h
+++ ios/SuperMap/AnimationWay.h
@@ -8,6 +8,30 @@
 
 #import <UIKit/UIKit.h>
 #import "AnimationGO.h"
+#import "Point3D.h"
+#include "Point3Ds.h"
+#import "Color.h"
+
+
 @interface AnimationWay : AnimationGO
 
+/**
+ * 路径动画的路径类型  0);//折线 1);//曲线
+ */
+@property(nonatomic)int pathType;
+@property(nonatomic)BOOL showPathTrack;
+@property(nonatomic)double trackLineWidth;
+@property(nonatomic,strong)Color* trackLineColor;
+
+//! \brief 路径动画是否沿轨迹切线方向。
+//! \param bPathTrack [in]路径动画是否沿轨迹切线方向。
+@property(nonatomic)BOOL pathTrackDir;
+
+-(BOOL)addPathPt:(Point3D)vecPath;
+-(BOOL)insertPathPt:(int)index pt:(Point3D)vecPath;
+-(BOOL)setPathPt:(int)index pt:(Point3D)vecPath;
+-(BOOL)removePathPtAt:(int)index;
+-(int)getPathPtCount;
+-(void)removeAllPathPt;
+-(Point3Ds*)getAllPathPt;
 @end
