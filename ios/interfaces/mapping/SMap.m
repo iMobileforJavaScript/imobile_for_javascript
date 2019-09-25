681d200d9a4831ed1f152febec64dbd7b1cf252d
DragonYang <dragonyang0917@gmail.com>
2019-09-25 15:10:07 +0800

    保存地图接口，新增是否保存工作空间的判断

--- ios/interfaces/mapping/SMap.m
+++ ios/interfaces/mapping/SMap.m
@@ -2577,7 +2577,7 @@ RCT_REMAP_METHOD(cancel, cancelWithResolver:(RCTPromiseResolveBlock)resolve reje
 }
 
 #pragma mark 保存地图 autoNaming为true的话若有相同名字的地图则自动命名
-RCT_REMAP_METHOD(saveMap, saveMapWithName:(NSString *)name autoNaming:(BOOL)autoNaming resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
+RCT_REMAP_METHOD(saveMap, saveMapWithName:(NSString *)name autoNaming:(BOOL)autoNaming saveWorkspace:(BOOL)saveWorkspace resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     @try {
         BOOL mapSaved = NO;
         BOOL wsSaved = NO;
@@ -2604,9 +2604,11 @@ RCT_REMAP_METHOD(saveMap, saveMapWithName:(NSString *)name autoNaming:(BOOL)auto
         } else {
             mapSaved = [map save:_name];
         }
-        wsSaved = [[SMap singletonInstance].smMapWC.workspace save];
+        if (mapSaved && saveWorkspace) {
+            wsSaved = [[SMap singletonInstance].smMapWC.workspace save];
+        }
         
-        if (mapSaved && wsSaved) {
+        if (mapSaved && (!saveWorkspace || wsSaved)) {
             resolve(_name);
         } else {
             resolve([NSNumber numberWithBool:mapSaved && wsSaved]);
@@ -2619,7 +2621,7 @@ RCT_REMAP_METHOD(saveMap, saveMapWithName:(NSString *)name autoNaming:(BOOL)auto
 #pragma mark 移除指定位置的地图
 RCT_REMAP_METHOD(removeMapByIndex, removeMapWithIndex:(int)index resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     @try {
-        BOOL result = NO;
+        BOOL result = YES;
         Maps* maps = SMap.singletonInstance.smMapWC.workspace.maps;
         if (maps.count > 0 && index < maps.count) {
             if (index < 0) {
@@ -2648,7 +2650,7 @@ RCT_REMAP_METHOD(removeMapByIndex, removeMapWithIndex:(int)index resolver:(RCTPr
 #pragma mark 移除指定名称的地图
 RCT_REMAP_METHOD(removeMapByName, removeMapWithName:(NSString *)name resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     @try {
-        BOOL result = NO;
+        BOOL result = YES;
         Maps* maps = SMap.singletonInstance.smMapWC.workspace.maps;
         if (maps.count > 0 && (name == nil || [name isEqualToString:@""])) {
             for (int i = 0; i < maps.count; i++) {
